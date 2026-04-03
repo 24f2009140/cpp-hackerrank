bdb16b9642cd61be8043d9bffacaf067f004dae4
VADREVU ANIRUDH <157278006+24f2009140@users.noreply.github.com>
2026-04-03 23:47:33 +0530

    Create datatypes.cpp

---
+++ datatypes.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    int a;
+    long b;
+    char c;
+    float d;
+    double e;
+    cin >> a;
+    cin >> b;
+    cin >> c;
+    cin >> d;
+    cin >> e;
+    printf("%d\n", a);
+    printf("%ld\n", b);
+    printf("%c\n", c);
+    printf("%.3f\n", d);
+    printf("%.9lf\n", e);
+    return 0;
+}
